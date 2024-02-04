1a1ce65b20f715c8f8b12acc5aea3befd46587cb
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-02-05 01:06:17 +0600

    use set state and go time and setting time.mounth ctlc press ttake click mounth setsate give cuurent time

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-      
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -25,8 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
- 
-
   final String title;
 
   @override
@@ -35,37 +32,28 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
  
-
   @override
   Widget build(BuildContext context) {
- 
+     var time = DateTime.now();
     return Scaffold(
       appBar: AppBar(
-        
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
         title: Text(widget.title),
       ),
       body: Center(
-        
-        child: Card(
-          color: const Color.fromARGB(255, 111, 168, 214),
-          shadowColor: const Color.fromARGB(31, 247, 8, 8),
-            elevation: 16, // implement shadow color
-          child:Padding(
-            padding:EdgeInsets.all(8),
-            child:Container(
-              width: 100,
-              height: 100,
-              child: Center(
-                child: Text("hello m")
-              )
-            ),
-           ) ,
-        ),
+        child:Column(
+         
+          children: [
+            Text("${time}"),
+            ElevatedButton(onPressed: (){
+              setState(() {
+                
+              });
+            }, 
+            child: Text("click"))
+          ],
        
-      ),
- 
+      )),
     );
   }
 }
