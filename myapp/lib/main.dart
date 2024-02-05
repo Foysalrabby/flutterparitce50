d446d9259924317d206787d669432ae69af6c904
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-02-05 22:33:22 +0600

    when i click login buton take value  textfield

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -31,10 +31,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
- 
   @override
   Widget build(BuildContext context) {
-     var time = DateTime.now();
+    var time = DateTime.now();
+    var emailtext = TextEditingController();
+    var passwordltext = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -43,66 +44,52 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Container(
           width: 300,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-         TextField(
-            decoration: InputDecoration(
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20),
-                   borderSide: BorderSide(
-                      color: Color.fromARGB(255, 161, 137, 30),
-                      width: 2
-                      )  
-                    ),
-                 enabledBorder: OutlineInputBorder(
-                   borderRadius: BorderRadius.circular(20),
-                   borderSide: BorderSide(
-                      color: Color.fromARGB(255, 161, 137, 30),
-                      width: 2
-                      ),
-                      
-                    ),
-                    suffixText: "username", //input last use suffix
-                  suffixIcon: IconButton(
-                    icon:Icon(Icons.remove_red_eye),
-                    onPressed: (){
-
-                    },
-                    
-                    ),
-                    prefixIcon: Icon(Icons.email,color: Colors.red,) //samne bose
-                  
-                  
-                 
-                  
-
-
-
-              // border:OutlineInputBorder(
-              //       borderRadius: BorderRadius.circular(20),
-              //       borderSide: BorderSide(color: Color.fromARGB(255, 161, 137, 30))
-              // )
-             ),
-          ),
-          Container(
-            height: 10, 
-          ),
-
-          TextField(
-            decoration: InputDecoration(
-              border:OutlineInputBorder(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            TextField(
+              controller: emailtext,
+              decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(
+                          color: Color.fromARGB(255, 161, 137, 30), width: 2)),
+                  enabledBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(20),
-                    borderSide: BorderSide(color: Color.fromARGB(255, 161, 137, 30))
-              ),
-              hintText: "enter password"
-             ),
-          )
+                    borderSide: BorderSide(
+                        color: Color.fromARGB(255, 161, 137, 30), width: 2),
+                  ),
+                  suffixText: "username", //input last use suffix
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.remove_red_eye),
+                    onPressed: () {},
+                  ),
+                  prefixIcon: Icon(
+                    Icons.email,
+                    color: Colors.red,
+                  ) //samne bose
 
-            ]
+                  // border:OutlineInputBorder(
+                  //       borderRadius: BorderRadius.circular(20),
+                  //       borderSide: BorderSide(color: Color.fromARGB(255, 161, 137, 30))
+                  // )
+                  ),
             ),
-          
-         
+            Container(
+              height: 10,
+            ),
+            TextField(
+              controller: passwordltext,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide:
+                          BorderSide(color: Color.fromARGB(255, 161, 137, 30))),
+                  hintText: "enter password"),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 7),
+              child: ElevatedButton(onPressed: () {}, child: Text("Login")),
+            )
+          ]),
         ),
       ),
     );
