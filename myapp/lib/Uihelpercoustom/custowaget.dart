caba11c8ba112ac4642a23a482a601f0318e304d
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-02-09 00:55:59 +0600

    create uihelper folder and custom.dart fil;e to call body

---
+++ myapp/lib/Uihelpercoustom/custowaget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class custombutton extends StatelessWidget {
+  // final buttonmane=required;
+  // final icon
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: () {}, child: Text("ROUNDED"));
+  }
+}
--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -33,117 +33,17 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    var arrycolor = [
-      Color.fromARGB(255, 95, 16, 60),
-      Color.fromARGB(255, 124, 42, 42),
-      Color.fromARGB(255, 11, 192, 86),
-      Color.fromARGB(255, 4, 42, 126)
-    ];
+    
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: Text(widget.title),
         ),
-        body: Container(
-          padding: EdgeInsets.all(8),
-          child: Column(children: [
-            Expanded(
-              flex: 2,
-              child: Container(
-                height: 150,
-                color: Color.fromARGB(255, 55, 136, 202),
-                child: ListView.builder(
-                  itemBuilder: (context, index) => Padding(
-                    padding: EdgeInsets.all(8),
-                    child: SizedBox(
-                      width: 100,
-                      child: CircleAvatar(
-                        backgroundColor: Colors.red,
-                      ),
-                    ),
-                  ),
-                  itemCount: 5,
-                  scrollDirection: Axis.horizontal,
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 4,
-              child: Container(
-                height: 200,
-                color: Color.fromARGB(255, 109, 21, 136),
-                child: ListView.builder(
-                  itemBuilder: (context, index) {
-                    return (ListTile(
-                      leading: CircleAvatar(
-                        backgroundColor: Colors.amber, //leading sit first
-                        child: Icon(Icons.add_a_photo),
-                      ),
-                      title: Text("Lorem is good"), //tittle second
-                      trailing: Text(
-                        "#",
-                        style: TextStyle(color: Colors.white),
-                      ), //last bose
-                    ));
-                  },
-                  itemCount: 10,
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 2,
-              child: Container(
-                  height: 200,
-                  color: Color.fromARGB(255, 233, 236, 43),
-                  child: Padding(
-                    padding: EdgeInsets.all(6),
-                    child: GridView.builder(
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 1),
-                      itemBuilder: (context, index) {
-                        return Container(
-                          color: arrycolor[index],
-                        );
-                      },
-                      itemCount: arrycolor.length,
-                      scrollDirection: Axis.horizontal,
-                    ),
-                  )),
-            ),
-            Craditems(),  //to call
-            Expanded(
-              flex: 1,
-              child: Container(
-                height: 150,
-                color: Color.fromARGB(255, 102, 214, 27),
-              ),
-            ),
-          ]),
+        body: Center(
+           child: ElevatedButton(onPressed: (){
+
+           },child: Text("click"),),
         ));
   }
 }
 
-class Craditems extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return 
-       Expanded(
-              flex: 2,
-              child: Container(
-                height: 150,
-                color: Color.fromARGB(255, 235, 85, 16),
-                margin: EdgeInsets.only(top: 4, bottom: 4),
-                child: Card(
-                  child: Container(
-                    width: 100,
-                    height: 50,
-                    color: Colors.amber,
-                    child: Text("heello"),
-                  ),
-                ),
-              ),
-            );
-
-   
-  }
-}
